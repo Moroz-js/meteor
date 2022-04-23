ea3aa0b148dd8b9de7c455e36f9568c8bf148e6f
SimonVerb <turboezikda@gmail.com>
2022-04-24 02:39:54 +0300

    адаптив + попап

--- src/js/main.js
+++ src/js/main.js
@@ -19,11 +19,14 @@ for (i = 0; i < acc.length; i++) {
   });
 };
 
-
 new Swiper('.swiper', {
   navigation: {
     nextEl: '.swiper-btn',
   },
+  slidesPerView: 3,
+
+  centeredSlides: true,
+  spaceBetween: 30,
   loop: true,
 });
 
@@ -31,9 +34,102 @@ new Swiper('.swiper', {
 
 
 
+let modalFeedback = document.querySelector('.modal__feedback');
+modalFeedback.addEventListener('click', () => {
+  modalFeedback.classList.remove('active')
+})
+
+function openFeedback() {
+  modalFeedback.classList.add('active')
+
+}
+$('.phone-field').inputmask("+7(999)999-9999");
 
 
+jQuery.validator.addMethod("checkMaskPhone", function (value, element) {
+  return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value);
+});
+
+$("#feedback").validate({
+  rules: {
+    name: "required",
+    email: {
+      required: true,
+      email: true
+    },
+    message: {
+      required: true,
+
+    },
+    agree: {
+      required: true
+    },
+    phone: {
+      checkMaskPhone: true,
+
+    }
+  },
+  messages: {
+    email: 'Неверно введён e-mail',
+    name: 'Необходимо заполнить поле',
+    agree: 'Необходимо дать согласие',
+    message: 'Необходимо заполнить поле',
+    phone: 'Введите корректный номер телефона'
+
+  },
+
+  submitHandler: function (form, event) {
+    event.preventDefault();
+
+    openFeedback()
+    document.getElementById("feedback").reset();
+
+  }
+});
+document.querySelector('.btn-form').addEventListener('click', () => {
+  $("#feedback").submit()
+})
 
 
+let list = document.querySelector('.cost__adaptive-all');
+let itemsTitle = document.querySelectorAll('.cost__adaptive-name-title');
+let firstTitle = document.querySelector('.first-title');
+let items = document.querySelectorAll('.cost__adaptive-item');
+let firstItem = document.querySelector('.first-item');
+let titles = document.querySelectorAll('.cost__adaptive-name-title');
+let listItems = document.querySelectorAll('.cost__adaptive-all-block')
+document.addEventListener('DOMContentLoaded', () => {
+  firstTitle.classList.add('visible');
+  firstItem.classList.add('visible')
+  list.classList.remove('visible')
+})
+
+titles.forEach((item, i) => {
+  item.addEventListener('click', (e) => {
+
+    itemsTitle.forEach(item => {
+      item.classList.remove('visible')
+    })
+    items.forEach(item => {
+      item.classList.remove('visible')
+    })
+    list.classList.add('visible')
+    e.target.classList.add('visible')
+
+  })
+});
+listItems.forEach((item, i) => {
+  item.addEventListener('click', () => {
+    list.classList.remove('visible')
+    items[i].classList.add('visible')
+    firstTitle.classList.remove('visible')
+    itemsTitle.forEach((item, i) => {
+      item.classList.remove('visible')
+    });
+
+    itemsTitle[i].classList.add('visible')
+
+  })
+});
 
 
